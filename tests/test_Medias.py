2d85b07e54320f8387b4f6931bc8db8395e72e22
Thomas Margnac <t.margnac@gmail.com>
2022-11-26 15:33:56 +0100

    test(test_Media): Improving tests with global variables

--- tests/test_Medias.py
+++ tests/test_Medias.py
@@ -4,10 +4,16 @@ import pytest
 import hashlib
 import json
 
+# Global variables
+
+test_video = "../media/test_video.mp4"
+test_video_audio = "../media/test_video_with_audio.mp4"
+#test_video_subtitled = "../media/test_video_subtitled.mp4"
+
 # Media testings
 
 def test_media_metadata():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	expected = json.loads(open("../media/test_media_metadata.txt", "r").read())
 	generated = video.getMetadata()
 	assert expected["streams"] == generated["streams"]
@@ -15,21 +21,21 @@ def test_media_metadata():
 # Video testings
 
 def test_video_loop():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	vlooped = video.loop(40)
 	expected = open("../media/test_video_looped.txt", "r").read()
 	generated = open(vlooped._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_clip():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	vcliped = video.clip(0, 10)
 	expected = open("../media/test_video_cliped.txt", "r").read()
 	generated = open(vcliped._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_add_audio_replace():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	audio = Audio("../media/test_audio.mp3")
 	video.addAudio(audio, "replace")
 	expected = open("../media/test_video_add_audio_replace.txt", "r").read()
@@ -37,7 +43,7 @@ def test_video_add_audio_replace():
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_add_audio_add():
-	video = Video("../media/test_video_with_audio.mp4")
+	video = Video(test_video_audio)
 	audio = Audio("../media/test_audio.mp3")
 	video.addAudio(audio, "add")
 	expected = open("../media/test_video_add_audio_add.txt", "r").read()
@@ -45,7 +51,7 @@ def test_video_add_audio_add():
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_add_audio_combine():
-	video = Video("../media/test_video_with_audio.mp4")
+	video = Video(test_video_audio)
 	audio = Audio("../media/test_audio.mp3")
 	video.addAudio(audio, "combine")
 	expected = open("../media/test_video_add_audio_combine.txt", "r").read()
@@ -53,62 +59,62 @@ def test_video_add_audio_combine():
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_add_audio_silent_replace():
-	video = Video("../media/test_video_with_audio.mp4")
+	video = Video(test_video_audio)
 	video.addAudio(None, type="silent")
 	expected = open("../media/test_video_add_audio_silent_replace.txt", "r").read()
 	generated = open(video._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_add_audio_silent():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	video.addAudio(None, type="silent")
 	expected = open("../media/test_video_add_audio_silent.txt", "r").read()
 	generated = open(video._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_remove_audio():
-	video = Video("../media/test_video_with_audio.mp4")
+	video = Video(test_video_audio)
 	video.removeAudio()
 	expected = open("../media/test_video_remove_audio.txt", "r").read()
 	generated = open(video._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_convert():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	video.convert("mov")
 	expected = open("../media/test_video_convert.txt", "r").read()
 	generated = open(video._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_change_volume():
-	video = Video("../media/test_video_with_audio.mp4")
+	video = Video(test_video_audio)
 	video.changeVolume(0.5)
 	expected = open("../media/test_video_change_volume.txt", "r").read()
 	generated = open(video._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_resize_simple():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	video.resize(720, 480, "simple")
 	expected = open("../media/test_video_resize_simple.txt", "r").read()
 	generated = open(video._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_resize_aspect_ratio():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	video.resize(720, 480, "aspect_ratio")
 	expected = open("../media/test_video_resize_aspect_ratio.txt", "r").read()
 	generated = open(video._main_temp, "rb").read()
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_video_change_frame_rate():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	video.changeFrameRate(60)
 	frame_rate = video.getMetadata()['streams'][0]['r_frame_rate']
 	assert frame_rate == '60/1'
 
 def test_add_subtitles_srt_hard():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	subtitles = Subtitles("../media/test_subtitles.srt")
 	video.addSubtitles(subtitles, "hard")
 	expected = open("../media/test_add_subtitles_srt_hard.txt", "r").read()
@@ -116,7 +122,7 @@ def test_add_subtitles_srt_hard():
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_add_subtitles_ass_hard():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	subtitles = Subtitles("../media/test_subtitles.ass")
 	video.addSubtitles(subtitles, "hard")
 	expected = open("../media/test_add_subtitles_ass_hard.txt", "r").read()
@@ -124,7 +130,7 @@ def test_add_subtitles_ass_hard():
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_add_subtitles_srt_soft():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	subtitles = Subtitles("../media/test_subtitles.srt")
 	video.addSubtitles(subtitles, "soft", 0, "eng")
 	expected = open("../media/test_add_subtitles_soft.txt", "r").read()
@@ -132,7 +138,7 @@ def test_add_subtitles_srt_soft():
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_add_subtitles_ass_soft():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	subtitles = Subtitles("../media/test_subtitles.ass")
 	video.addSubtitles(subtitles, "soft", 0, "eng")
 	expected = open("../media/test_add_subtitles_soft.txt", "r").read()
@@ -140,7 +146,7 @@ def test_add_subtitles_ass_soft():
 	assert hashlib.sha512(generated).hexdigest() == expected
 
 def test_add_text():
-	video = Video("../media/test_video.mp4")
+	video = Video(test_video)
 	texts = [Text("First Text", x="(w-text_w)/2", y="(h-text_h)/2", start=0, end=10, fontSize=24),Text("Second Text", x="(w-text_w)/2", y="(h-text_h)/2", start=10, end=20, fontSize=24)]
 	video.addText(texts=texts)
 	expected = open("../media/test_add_text.txt", "r").read()
